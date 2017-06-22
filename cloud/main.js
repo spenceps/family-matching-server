87759f23a32d3d675363c117bb46f8e6ad84ab27
Spencer Smith <spencer@madewithstudio.com>
2017-06-22 07:08:29 -0600

    running

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,19 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+
+Parse.Cloud.beforeSave("GameResult", function(request, response)  {
+
+	var query = new Parse.Query("GameResult");
+	query.equalTo("person",request.object.get("person"));
+	query.equalTo("game",request.object.get("game"));
+	query.limit(1000);
+	console.log("about to make query");
+	query.find().then(function(previousResults) {
+		request.object.set("gameCount",previousResults.length + 1);
+		response.success();
+	}, function(error) {
+		response.success();
+	});
+});
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 // Example express application adding the parse-server module to expose Parse
 // compatible API routes.
 
+// foreman start -f ProcfileNodemon -p 5001 web
+
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
+var ParseDashboard = require('parse-dashboard');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
 
@@ -11,15 +14,29 @@ if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
+var allowInsecureHTTP = true;
+var dashboard = new ParseDashboard({
+  "apps": [
+    {
+      "serverURL": process.env.SERVER_URL || 'http://192.168.1.114:' + 5001 + '/parse',
+      "appId": process.env.APP_ID ,
+      "masterKey": process.env.MASTER_KEY,
+      "appName": "Family Matching"
+    }],
+    "users": [
+    {
+      "user":process.env.DASHBOARD_USER,
+      "pass":process.env.DASHBOARD_PASS
+    },
+  ]
+},allowInsecureHTTP);
+
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/family',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
-  }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
@@ -45,10 +62,12 @@ app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
+app.use('/dashboard', dashboard);
+
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
-    console.log('parse-server-example running on port ' + port + '.');
+    console.log('family-matching-server running on port ' + port + '.');
 });
 
 // This will enable the Live Query real-time server
